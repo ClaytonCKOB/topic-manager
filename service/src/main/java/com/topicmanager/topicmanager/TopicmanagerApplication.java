8d4aefb83ed48155b6527a204d79da0269548eab
Clayton Barcelos <clayton.barcelos@sap.com>
2025-09-06 15:33:23 -0300

    init project

---
+++ service/src/main/java/com/topicmanager/topicmanager/TopicmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.topicmanager.topicmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicmanagerApplication.class, args);
+	}
+
+}
---
+++ service/src/test/java/com/topicmanager/topicmanager/TopicmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topicmanager.topicmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
