2cb9164009832c68017e0a2e3b1a013f5fcf6980
Clayton Barcelos <claytonckob@gmail.com>
2025-09-06 18:42:19 -0300

    feat: using migrations with flyway

---
+++ service/src/main/resources/db/migration/V1__init_definitions.sql
@@ -0,0 +1,44 @@
+CREATE TABLE meeting_status (
+    meeting_status_id SERIAL PRIMARY KEY,
+    description VARCHAR NOT NULL
+);
+
+CREATE TABLE user_account (
+	user_account_id SERIAL PRIMARY KEY,
+	username VARCHAR NOT NULL,
+	email VARCHAR NOT NULL UNIQUE,
+	password VARCHAR NOT NULL,
+	role INT NOT NULL,
+	active INT NOT NULL,
+	created_date TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE meeting (
+    meeting_id SERIAL PRIMARY KEY,
+    meeting_status_id INT NOT NULL REFERENCES meeting_status(meeting_status_id),
+    title VARCHAR NOT NULL,
+    description VARCHAR,
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL
+);
+
+CREATE TABLE meeting_topic (
+    meeting_topic_id SERIAL PRIMARY KEY,
+    meeting_id INT NOT NULL REFERENCES meeting(meeting_id) ON DELETE CASCADE,
+    title VARCHAR NOT NULL,
+    description VARCHAR
+);
+
+CREATE TABLE meeting_participant (
+    user_account_id INT NOT NULL REFERENCES user_account(user_account_id) ON DELETE CASCADE,
+    meeting_id INT NOT NULL REFERENCES meeting(meeting_id) ON DELETE CASCADE,
+    PRIMARY KEY (user_account_id, meeting_id)
+);
+
+CREATE TABLE meeting_vote (
+	meeting_vote_id SERIAL PRIMARY KEY,
+	meeting_id INT NOT NULL REFERENCES meeting(meeting_id) ON DELETE CASCADE,
+	meeting_topic_id INT NOT NULL REFERENCES meeting_topic(meeting_topic_id) ON DELETE CASCADE,
+	user_account_id INT NOT NULL REFERENCES user_account(user_account_id) ON DELETE CASCADE,
+	updated_date TIMESTAMP NOT NULL DEFAULT NOW()
+);
