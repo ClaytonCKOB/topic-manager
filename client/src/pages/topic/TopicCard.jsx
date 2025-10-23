735c8bc0e8d490746982a8d803c6a7e2329a22f8
Clayton Barcelos <claytonckob@gmail.com>
2025-10-22 22:36:57 -0300

    feat: creating TopicComponent

--- client/src/pages/topic/TopicCard.jsx
+++ client/src/pages/topic/TopicCard.jsx
@@ -1,230 +1,25 @@
-import { Typography, Button, TextField, Grid, IconButton, Input, List, ListItem, ListItemText } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import AttachFileIcon from '@mui/icons-material/AttachFile';
-import FileList from "../../base/components/files/FileList";
+import { Grid} from "@mui/material";
+import TopicComponent from "./TopicComponent";
 
 export default function TopicCard({meeting, setMeeting, topic, index, isEditable}) {
-
-  const onRemoveTopic = (index) => {
-      setMeeting((prev) => ({
-      ...prev,
-      topics: prev.topics.filter((_, i) => i !== index),
-      }));
-  };
-
-  const onChangeTopicTitle = (index, newTitle) => {
-      setMeeting((prev) => ({
-      ...prev,
-      topics: prev.topics.map((t, i) =>
-          i === index ? { ...t, title: newTitle } : t
-      ),
-      }));
-  };
-
-  const onAddSubTopic = (index) => {
-      setMeeting((prev) => ({
-      ...prev,
-      topics: prev.topics.map((t, i) =>
-          i === index
-          ? { ...t, subtopics: [...(t.subtopics || []), { title: "", files: [] }] }
-          : t
-      ),
-      }));
-  };
-
-  const onRemoveSubTopic = (index, subIndex) => {
-      setMeeting((prev) => ({
-      ...prev,
-      topics: prev.topics.map((t, i) =>
-          i === index
-          ? { ...t, subtopics: t.subtopics.filter((_, j) => j !== subIndex) }
-          : t
-      ),
-      }));
-  };
-
-  const onChangeSubTopicTitle = (index, subIndex, newTitle) => {
-      setMeeting((prev) => ({
-      ...prev,
-      topics: prev.topics.map((t, i) =>
-          i === index
-          ? {
-              ...t,
-              subtopics: t.subtopics.map((s, j) =>
-                  j === subIndex ? { ...s, title: newTitle } : s
-              ),
-              }
-          : t
-      ),
-      }));
-  };
-
-  const onAddTopicFiles = (index, fileList) => {
-    const files = Array.from(fileList);
-    setMeeting((prev) => ({
-      ...prev,
-      topics: prev.topics.map((t, i) =>
-        i === index ? { ...t, files: [...(t.files || []), ...files] } : t
-      ),
-    }));
-  };
-
-  const onRemoveTopicFile = (index, fileIndex) => {
-      setMeeting((prev) => ({
-      ...prev,
-      topics: prev.topics.map((t, i) =>
-          i === index
-          ? { ...t, files: t.files.filter((_, j) => j !== fileIndex) }
-          : t
-      ),
-      }));
-  };
-
-  const onAddSubtopicFiles = (index, subIndex, fileList) => {
-    const files = Array.from(fileList);
-    setMeeting((prev) => ({
-      ...prev,
-      topics: prev.topics.map((t, i) =>
-        i === index
-          ? {
-              ...t,
-              subtopics: t.subtopics.map((s, j) =>
-                j === subIndex
-                  ? { ...s, files: [...(s.files || []), ...files] }
-                  : s
-              ),
-            }
-          : t
-      ),
-    }));
-  };
-
-  const onRemoveSubtopicFile = (index, subIndex, fileIndex) => {
-      setMeeting((prev) => ({
-      ...prev,
-      topics: prev.topics.map((t, i) =>
-          i === index
-          ? {
-              ...t,
-              subtopics: t.subtopics.map((s, j) =>
-                  j === subIndex
-                  ? { ...s, files: s.files.filter((_, k) => k !== fileIndex) }
-                  : s
-              ),
-              }
-          : t
-      ),
-      }));
-  };
-
-
   return (
     <Grid key={index}>
-      <Grid item mb={3} p={3} sx={{ border: '1px solid gray', borderRadius: 2 }}>
-        <Grid sx={{ display: "flex", justifyContent: "space-between" }}>
-          <Typography variant="h6" mb={2}>
-            Pauta {index + 1}
-          </Typography>
-          {
-            isEditable ? 
-            <Grid>
-              <IconButton onClick={() => onAddSubTopic(index)}>
-                <AddCircleOutlineIcon />
-              </IconButton>
-              <IconButton color="error" size="small" onClick={() => onRemoveTopic(index)}>
-                <DeleteIcon />
-              </IconButton>
-            </Grid>
-            :<></>
-          }
-        </Grid>
-
-        <Grid>
-          <TextField
-            multiline
-            minRows={2}
-            placeholder="Adicione a descrição da pauta..."
-            fullWidth
-            value={topic.title}
-            disabled={!isEditable}
-            onChange={(e) => onChangeTopicTitle(index, e.target.value)}
-          />
-        </Grid>
-        <FileList 
-          files={topic.files}
-          isEditable={isEditable}
-          removeFile={(fIndex) => {onRemoveTopicFile(index, fIndex)}}
-        />
-
-        {
-          isEditable ?
-          <Grid sx={{ display: 'flex', justifyContent: 'flex-end', paddingTop: 2 }}>
-            <Button component="label" variant="contained" startIcon={<AttachFileIcon />}>
-              Adicionar Anexo(s)
-              <Input
-                type="file"
-                inputProps={{ accept: 'application/pdf' }}
-                onChange={(e) => onAddTopicFiles(index, e.target.files)}
-                sx={{ display: 'none' }}
-                multiple
-              />
-            </Button>
-          </Grid>
-          : <></>
-        }
-      </Grid>
-
+      <TopicComponent
+        setMeeting={setMeeting}
+        topic={topic}
+        index={index}
+        isEditable={isEditable}
+      />
       <Grid sx={{ display: 'flex', alignItems: 'end', flexDirection: 'column' }}>
         {topic.subtopics?.map((subtopic, sub_index) => (
-          <Grid item key={sub_index} mb={3} p={3} sx={{ border: '1px solid gray', borderRadius: 2, width: 0.9 }}>
-            <Grid sx={{ display: "flex", justifyContent: "space-between" }}>
-              <Typography variant="h6" mb={2}>
-                Pauta {index + 1}.{sub_index + 1}
-              </Typography>
-              {
-                isEditable ?
-                <Grid>
-                  <IconButton color="error" size="small" onClick={() => onRemoveSubTopic(index, sub_index)}>
-                    <DeleteIcon />
-                  </IconButton>
-                </Grid>
-                : <></>
-              }
-            </Grid>
-
-            <TextField
-              disabled={!isEditable}
-              multiline
-              minRows={2}
-              placeholder="Adicione a descrição da subpauta..."
-              fullWidth
-              value={subtopic.title}
-              onChange={(e) => onChangeSubTopicTitle(index, sub_index, e.target.value)}
-            />
-            <FileList
-              files={subtopic.files}
-              isEditable={isEditable}
-              removeFile={(fIndex) => {onRemoveSubtopicFile(index, sub_index, fIndex)}}
-            />
-
-            {
-              isEditable ?
-              <Grid sx={{ display: 'flex', justifyContent: 'flex-end', paddingTop: 2 }}>
-                <Button component="label" variant="contained" startIcon={<AttachFileIcon />}>
-                  Adicionar Anexo(s)
-                  <Input
-                    type="file"
-                    inputProps={{ accept: 'application/pdf' }}
-                    onChange={(e) => onAddSubtopicFiles(index, sub_index, e.target.files)}
-                    sx={{ display: 'none' }}
-                    multiple
-                  />
-                </Button>
-              </Grid>
-              : <></>
-            }
-          </Grid>
+          <TopicComponent 
+            setMeeting={setMeeting}
+            index={index}
+            subIndex={sub_index}
+            topic={subtopic}
+            isSubTopic={true}
+            isEditable={isEditable}
+          />
         ))}
       </Grid>
     </Grid>
---
+++ client/src/pages/topic/TopicComponent.jsx
@@ -0,0 +1,179 @@
+import { Typography, Button, TextField, Grid, IconButton, Input } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import FileList from "../../base/components/files/FileList";
+
+export default function TopicComponent({setMeeting, topic, index, subIndex, isEditable, isSubTopic}) {
+
+    const onAddSubTopic = (index) => {
+        setMeeting((prev) => ({
+        ...prev,
+        topics: prev.topics.map((t, i) =>
+            i === index
+            ? { ...t, subtopics: [...(t.subtopics || []), { title: "", files: [] }] }
+            : t
+        ),
+        }));
+    };
+
+    const onRemoveTopic = (index, subIndex) => {
+        if (isSubTopic) {
+            setMeeting((prev) => ({
+            ...prev,
+            topics: prev.topics.map((t, i) =>
+                i === index
+                ? { ...t, subtopics: t.subtopics.filter((_, j) => j !== subIndex) }
+                : t
+            ),
+            }));
+        } else {
+            setMeeting((prev) => ({
+            ...prev,
+            topics: prev.topics.filter((_, i) => i !== index),
+            }));
+        }
+    };
+
+    const onChangeTopicTitle = (index, subIndex, newTitle) => {
+        if (isSubTopic) {
+            setMeeting((prev) => ({
+            ...prev,
+            topics: prev.topics.map((t, i) =>
+                i === index
+                ? {
+                    ...t,
+                    subtopics: t.subtopics.map((s, j) =>
+                        j === subIndex ? { ...s, title: newTitle } : s
+                    ),
+                    }
+                : t
+            ),
+            }));
+        } else {
+            setMeeting((prev) => ({
+            ...prev,
+            topics: prev.topics.map((t, i) =>
+                i === index ? { ...t, title: newTitle } : t
+            ),
+            }));
+        }
+    };
+
+    const onAddTopicFiles = (index, subIndex, fileList) => {
+        if (isSubTopic) {
+            const files = Array.from(fileList);
+            setMeeting((prev) => ({
+            ...prev,
+            topics: prev.topics.map((t, i) =>
+                i === index
+                ? {
+                    ...t,
+                    subtopics: t.subtopics.map((s, j) =>
+                        j === subIndex
+                        ? { ...s, files: [...(s.files || []), ...files] }
+                        : s
+                    ),
+                    }
+                : t
+            ),
+            }));
+        } else {
+            const files = Array.from(fileList);
+            setMeeting((prev) => ({
+            ...prev,
+            topics: prev.topics.map((t, i) =>
+                i === index ? { ...t, files: [...(t.files || []), ...files] } : t
+            ),
+            }));
+        }
+    };
+
+    const onRemoveTopicFile = (index, subIndex, fileIndex) => {
+        if (isSubTopic) {
+            setMeeting((prev) => ({
+            ...prev,
+            topics: prev.topics.map((t, i) =>
+                i === index
+                ? {
+                    ...t,
+                    subtopics: t.subtopics.map((s, j) =>
+                        j === subIndex
+                        ? { ...s, files: s.files.filter((_, k) => k !== fileIndex) }
+                        : s
+                    ),
+                    }
+                : t
+            ),
+            }));
+        } else {
+            setMeeting((prev) => ({
+        ...prev,
+        topics: prev.topics.map((t, i) =>
+            i === index
+            ? { ...t, files: t.files.filter((_, j) => j !== fileIndex) }
+            : t
+        ),
+        }));
+        }
+        
+    };
+
+    return <>
+    <Grid item key={subIndex ?? index} mb={3} p={3} sx={{ border: '1px solid gray', borderRadius: 2, width: isSubTopic ? 0.9 : 1 }}>
+        <Grid sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Typography variant="h6" mb={2}>
+            Pauta {index + 1}{isSubTopic ? "." + (subIndex + 1) : ""} 
+            </Typography>
+            {
+            isEditable ?
+            <Grid>
+                {
+                    !isSubTopic ?
+                    <IconButton onClick={() => onAddSubTopic(index)}>
+                        <AddCircleOutlineIcon />
+                    </IconButton>
+                    : null
+                }
+                <IconButton color="error" size="small" onClick={() => onRemoveTopic(index, subIndex)}>
+                    <DeleteIcon />
+                </IconButton>
+            </Grid>
+            : <></>
+            }
+        </Grid>
+
+        <TextField
+            disabled={!isEditable}
+            multiline
+            minRows={2}
+            placeholder="Adicione a descrição da subpauta..."
+            fullWidth
+            value={topic.title}
+            onChange={(e) => onChangeTopicTitle(index, subIndex, e.target.value)}
+        />
+        <FileList
+            files={topic.files}
+            isEditable={isEditable}
+            removeFile={(fIndex) => {onRemoveTopicFile(index, subIndex, fIndex)}}
+        />
+
+        {
+            isEditable ?
+            <Grid sx={{ display: 'flex', justifyContent: 'flex-end', paddingTop: 2 }}>
+            <Button component="label" variant="contained" startIcon={<AttachFileIcon />}>
+                Adicionar Anexo(s)
+                <Input
+                type="file"
+                inputProps={{ accept: 'application/pdf' }}
+                onChange={(e) => onAddTopicFiles(index, subIndex, e.target.files)}
+                sx={{ display: 'none' }}
+                multiple
+                />
+            </Button>
+            </Grid>
+            : <></>
+        }
+        </Grid>
+    </>;
+}
