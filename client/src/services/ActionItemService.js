40f6080e11a160e08b5091b9203bde0d8dd87ca4
Clayton Barcelos <claytonckob@gmail.com>
2025-12-08 16:27:45 -0300

    feat: action item client service

---
+++ client/src/services/ActionItemService.js
@@ -0,0 +1,37 @@
+import request from '../utils/RequestUtil';
+
+export default class actionItemService {
+
+    async list(actorId) {
+        var response = {};
+        try {
+            let url = '/api/action-item/' + actorId;
+
+            response = await request.get(url);
+            if (response.status != 200) {
+                throw new Error('Network response was not ok');
+            }
+
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        } finally {
+            return response.data;
+        }
+    }
+
+    async complete(actionItemId) {
+        var response = {};
+        try {
+            response = await request.post('/api/action-item/' + actionItemId);
+
+            if (response.status != 200) {
+                throw new Error('Network response was not ok');
+            }
+
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        } finally {
+            return response.data;
+        }
+    }
+}
