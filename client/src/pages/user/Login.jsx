cf764420d51a5a7f41f891ef5bf0ad3c7e0fe4d0
Clayton Barcelos <claytonckob@gmail.com>
2025-12-08 13:45:59 -0300

    feat: design changes in the login page

--- client/src/pages/user/Login.jsx
+++ client/src/pages/user/Login.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthService from "../../services/AuthService";
-import { Backdrop, Box, Button, CircularProgress, IconButton, Skeleton, TextField, Typography, useMediaQuery } from "@mui/material";
+import { Box, Button, CircularProgress, IconButton, TextField, Typography, useMediaQuery } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUpRightFromSquare, faClose, faEye, faEyeSlash, faLinkSlash } from "@fortawesome/free-solid-svg-icons";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import ErrorMessage from "../../base/components/message/ErrorMessage";
 
 const Login = () => {
     const isSmallScreen = useMediaQuery('(max-width:600px) or (max-height:600px)');
-    const loginWidthPercentage = isSmallScreen ? 100 : 40;
+    const loginWidthPercentage = 100; //isSmallScreen ? 100 : 40;
     
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -68,65 +68,19 @@ const Login = () => {
     return (
         <Box
             sx={{
-                position: "absolute",
-                backgroundColor: "gray",
-                top: 0,
-                left: 0,
                 width: "100vw",
                 height: "100vh",
                 padding: 0,
-                margin: 0,
-                overflow: "hidden",
+                margin: 0
             }}
         >
-            <Backdrop 
-                open={serverOffline} 
-                sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                onClick={() => setServerOffline(false)}
-            >
-                <Box
-                    onClick={(e) => e.stopPropagation()}
-                    sx={{
-                        position: "relative",
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        backgroundColor: "white", 
-                        padding: "40px", 
-                        border: "1px solid",
-                        borderColor: "error.main",
-                        gap: "30px"
-                    }}
-                >
-                    <IconButton 
-                        onClick={() => setServerOffline(false)} 
-                        sx={{ position: "absolute", top: "10px", right: "10px", width: "40px", height: "40px" }}
-                    >
-                        <FontAwesomeIcon icon={faClose} />
-                    </IconButton>
-                    <Box>
-                        <Typography variant="h5" sx={{ color: "error.main", fontWeight: "bold" }}>
-                            Servidor offline
-                        </Typography>
-                        <Typography variant="body1" sx={{ color: "error.main", lineHeight: "1.5", marginTop: "10px" }}>
-                            O servidor não está respondendo. Por favor, tente novamente mais tarde.
-                        </Typography>
-                    </Box>
-                    <Typography variant="h2" sx={{ color: "error.main" }}>
-                        <FontAwesomeIcon icon={faLinkSlash} />
-                    </Typography>
-                </Box>
-            </Backdrop>
             <Box
                 sx={{
                     display: "flex",
                     justifyContent: "center",
-                    position: "absolute",
-                    backgroundColor: "white",
-                    top: 0,
-                    left: 0,
+                    alignItems: "center",
                     width: `${loginWidthPercentage}%`,
+                    backgroundColor: "#f5f5f5",
                     height: "100%",
                 }}
             >
@@ -134,57 +88,73 @@ const Login = () => {
                     sx={{
                         display: "flex",
                         flexDirection: "column",
-                        justifyContent: "center",
-                        maxWidth: "400px",
-                        width: "100%",
+                        justifyContent: "space-between",
+                        boxSizing: "border-box",
+                        maxWidth: "500px",
+                        height: "60%",
                         gap: "15px",
-                        padding: "2rem",
+                        padding: "3rem",
+                        alignItems: "center",
+                        borderRadius: "8px",
+                        boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+                        backgroundColor: "white",
                     }}
                 >
-                    <TextField
-                        autoFocus
-                        autoComplete="username"
-                        error={wrongCredentials}
-                        label="Usuário"
-                        variant="outlined"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        onFocus={() => setWrongCredentials(false)}
-                        onKeyDown={(e) => e.key === 'Enter' && handleLogin(e)}
-                        sx={{ width: "100%" }}
-                    />
-                    <TextField 
-                        error={wrongCredentials}
-                        label="Senha" 
-                        variant="outlined" 
-                        type={showPassword ? 'text' : 'password'}
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        onFocus={() => setWrongCredentials(false)}
-                        onKeyDown={(e) => e.key === 'Enter' && handleLogin(e)}
-                        sx={{ width: "100%" }}
-                        slotProps={{
-                            input: {
-                                endAdornment: (
-                                    <IconButton
-                                        onClick={() => setShowPassword(!showPassword)}
-                                        sx={{padding: '10px', height: '40px', width: '40px'}}
-                                    >
-                                        <FontAwesomeIcon
-                                            size="xs"
-                                            icon={showPassword ? faEye : faEyeSlash}
-                                            color={wrongCredentials ? 'darkred' : 'gray'}
-                                        />
-                                    </IconButton>
-                                ),
-                                autoComplete: 'new-password',
-                                form: {
-                                    autoComplete: 'off'
-                                }
-                            }
+                    <Typography
+                        variant="h4"
+                        sx={{
+                            fontWeight: "bold",
+                            textAlign: "center",
+                            marginBottom: "20px",
                         }}
-                        helperText={wrongCredentials ? "Usuário ou senha inválidos" : ""}
-                    />
+                    >
+                        Topic Manager
+                    </Typography>
+
+                    <Box>
+                        <TextField
+                            autoFocus
+                            autoComplete="username"
+                            error={wrongCredentials}
+                            label="Usuário"
+                            variant="outlined"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            onFocus={() => setWrongCredentials(false)}
+                            onKeyDown={(e) => e.key === 'Enter' && handleLogin(e)}
+                            sx={{ width: "100%" }}
+                        />
+                        <TextField 
+                            error={wrongCredentials}
+                            label="Senha"
+                            variant="outlined"
+                            type={showPassword ? "text" : "password"}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            onFocus={() => setWrongCredentials(false)}
+                            onKeyDown={(e) => e.key === "Enter" && handleLogin(e)}
+                            sx={{ width: "100%", marginTop: "20px" }}
+                            slotProps={{
+                                input: {
+                                    endAdornment: (
+                                        <IconButton
+                                            onClick={() => setShowPassword(!showPassword)}
+                                            sx={{ padding: "10px", height: "40px", width: "40px" }}
+                                        >
+                                            <FontAwesomeIcon
+                                                size="xs"
+                                                icon={showPassword ? faEye : faEyeSlash}
+                                                color={wrongCredentials ? "darkred" : "gray"}
+                                            />
+                                        </IconButton>
+                                    ),
+                                    autoComplete: "new-password",
+                                    form: { autoComplete: "off" },
+                                },
+                            }}
+                            helperText={wrongCredentials ? "Usuário ou senha inválidos" : ""}
+                        />
+                    </Box>
                     <Button
                         type="submit"
                         variant="contained"
@@ -197,33 +167,13 @@ const Login = () => {
                             color: "white",
                         }}
                     >
-                        {loading ? (
-                            <CircularProgress color="inherit" size={24} />
-                        ) : (
-                            "Entrar"
-                        )}
+                        {loading ? <CircularProgress color="inherit" size={24} /> : "Entrar"}
                     </Button>
                 </Box>
             </Box>
-            <Box
-                sx={{
-                    display: isSmallScreen ? "none" : "block",
-                    position: "absolute",
-                    top: 0,
-                    right: 0,
-                    width: `${100 - loginWidthPercentage}%`,
-                    height: "100%",
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                }}
-            >
-            </Box>
-            <ErrorMessage
-                snackbar={snackbar}
-                setSnackbar={setSnackbar}
-            />
+            <ErrorMessage snackbar={snackbar} setSnackbar={setSnackbar} />
         </Box>
-    )
+    );
 };
 
 export default Login;
