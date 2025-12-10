7407c93870e79b956e431a9546edace2d5a2a3ee
Clayton Barcelos <claytonckob@gmail.com>
2025-12-10 20:58:21 -0300

    feat: listing action items

--- client/src/base/components/action_item/ActionItemList.jsx
+++ client/src/base/components/action_item/ActionItemList.jsx
@@ -1,5 +1,6 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import ActionItemService from "../../../services/ActionItemService";
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
 import AuthService from "../../../services/AuthService";
 import { useEffect, useState } from "react";
 
@@ -20,15 +21,61 @@ export default function ActionItemList() {
     return <>
     {
         actionItemList.length > 0 ? 
-        <Box mt={2} textAlign="left">
+        <Box mt={2} textAlign="left" width={0.6}>
             <Box>
                 <Typography variant="h5" fontWeight="bold">
                 Tarefas Pendentes
                 </Typography>
             </Box>
-            <Box>
+            <Box mt={3} display="flex" gap={2} overflow={"scroll"} >
                 {actionItemList.map((item) => (
-                    <></>
+                    <Box
+                    sx={{
+                        backgroundColor: "#e3e9f0",
+                        borderRadius: 2,
+                        px: 3,
+                        py: 2.5,
+                        display: "flex",
+                        flexDirection: "column",
+                        transition: "0.2s",
+                        width: "300px !important",
+                    }}>
+                        <Box display="flex" justifyContent="space-between" alignItems="center">
+                            <Typography variant="h6" fontWeight="bold">
+                                {item.sender.name}
+                            </Typography>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                startIcon={<FullscreenIcon/>}
+                                onClick={() => {}}
+                                sx={{
+                                    boxSizing: "border-box",
+                                    width: "40px",
+                                    padding: "8px",
+                                    minWidth: "0px",
+                                    justifyContent: "center",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    margin: "0px",
+                                }}
+                            >
+                            </Button>
+                        </Box>
+                        <Box
+                        sx={{
+                            display: "-webkit-box",
+                            WebkitLineClamp: 5,
+                            WebkitBoxOrient: "vertical",
+                            overflow: "hidden",
+                            width: "100%",
+                        }}
+                        >
+                            <Typography width="300px" mt={1} variant="subtitle1" color="text.secondary" gutterBottom>
+                                {item.comment}
+                            </Typography>
+                        </Box>
+                    </Box>
                 ))}
             </Box>
         </Box>
