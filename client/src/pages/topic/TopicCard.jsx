7339131eab2b2181a8e30e696fb7f065424eeeb5
Clayton Barcelos <claytonckob@gmail.com>
2026-03-09 15:39:53 -0300

    fix: changing border style of sections in meeting creation

--- client/src/pages/topic/TopicCard.jsx
+++ client/src/pages/topic/TopicCard.jsx
@@ -1,4 +1,4 @@
-import { Grid, Box, Typography } from "@mui/material";
+import { Grid, Box } from "@mui/material";
 import { useState } from 'react';
 import TopicComponent from "./TopicComponent";
 
